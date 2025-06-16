674fbbd2322eabd2aacc21a60e02f6232a91a2b5
LilithNML <lilithmorningstar468@gmail.com>
2025-06-16 00:52:49 -0600

    Update script.js

--- haziel/script.js
+++ haziel/script.js
@@ -33,5 +33,5 @@ enterBtn.addEventListener('click', () => {
 
 // Redirigir al hacer clic en el nombre
 clickable.addEventListener('click', () => {
-  window.location.href = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c185aac5-57e4-4f00-aea4-208c73532aef/ddqlluf-25d5289c-7891-4e97-a1f5-443f8549f3e4.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2MxODVhYWM1LTU3ZTQtNGYwMC1hZWE0LTIwOGM3MzUzMmFlZlwvZGRxbGx1Zi0yNWQ1Mjg5Yy03ODkxLTRlOTctYTFmNS00NDNmODU0OWYzZTQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.WQfG5ur8TjqnmSYXczYkcONslJks4Xv8-asNig5RCrU"; // ← cámbialo por la URL de destino
+  window.location.href = "https://sendgb.com/zNcrBeWqFR5"; // ← URL de destino
 });
